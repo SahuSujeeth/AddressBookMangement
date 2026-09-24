4afee9573312ab943d1a4f38ce3ebf00e8e96cca
SahuSujeeth <sahusujeeth2004@gmail.com>
2026-09-24 18:54:13 +0000

    updated createContact

--- contact.c
+++ contact.c
@@ -32,7 +32,7 @@ void createContact(AddressBook *addressBook)
     printf("The name is : %s\n",addressBook->contacts->name);
     int i=0;
     int nameValidate = 0;
-    int length = strlen(addressBook->contacts->name);
+    int length_Name = strlen(addressBook->contacts->name);
     //printf("The lenght of the name %d\n",length);
     
     while(addressBook->contacts->name[i] != '\0')
@@ -48,10 +48,77 @@ void createContact(AddressBook *addressBook)
         }
         i++;
     }
-    if(nameValidate == 1 && length >= 2)
+    if(nameValidate == 1 && length_Name >= 2)
     {
-       printf("Nice name is valid");
-       printf("Now u can enter the phone number now \n");
+       printf("Enter the Phone Number:");
+       scanf("%s",addressBook->contacts->phone);
+       printf("The phone number is :%s\n",addressBook->contacts->phone);
+       int length_PhNo = strlen(addressBook->contacts->phone);
+       printf("%d\n",length_PhNo);
+       if(length_PhNo == 10 && addressBook->contacts->phone[0] >= '6' && addressBook->contacts->phone[0] <= '9')
+       {
+        // printf("Phone number is valid\n");
+        printf("Enter mail id:");
+        scanf("%s",addressBook->contacts->email);
+        int lenght_mail = strlen(addressBook->contacts->email);
+        int mailValidate = 1;
+        int i = 0;
+        int atCount = 0;
+        int atPosition = -1;
+        while(addressBook->contacts->email[i] != '\0')
+        {
+            if(addressBook->contacts->email[i] >= 'A' && addressBook->contacts->email[i] <= 'Z')
+            {
+                mailValidate = 0;
+                break;
+            }
+            if(addressBook->contacts->email[i] == '@')
+            {
+                atCount++;
+                atPosition = i;
+            }
+            i++;
+        }
+        if(atCount != 1)
+        {
+            mailValidate = 0;
+        }
+        if(addressBook->contacts->email[0] == '@')
+        {
+            mailValidate = 0;
+        }
+        if(lenght_mail < 4)
+        {
+            mailValidate = 0;
+        }
+        else
+        {
+            if(addressBook->contacts->email[lenght_mail - 4] != '.' || addressBook->contacts->email[lenght_mail - 3] != 'c' || addressBook->contacts->email[lenght_mail - 2] != 'o' || addressBook->contacts->email[lenght_mail - 1] != 'm')
+            {
+                mailValidate = 0;
+            }
+        }
+        if(atPosition + 1 >= lenght_mail - 4)
+        {
+            mailValidate = 0;
+        }
+        if(mailValidate == 1)
+        {
+            printf("Mail is valid\n");
+            
+        }
+        else
+        {
+            printf("Mail is not valid\n");
+            
+        }
+
+       }
+       else
+       {
+        printf("Phone number is not valid\n");
+        
+       }  
     }
     else
     {
